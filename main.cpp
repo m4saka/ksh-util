57862037dae71d512bbe641d20b86e3da47874b1
masaka <masaka@mz-kb.com>
2018-10-03 17:30:58 +0900

    feat: add getting dropped filenames

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
+#include <iostream>
 #include <nana/gui/wvl.hpp>
+#include <URLParser.hpp>
 
 int main()
 {
     nana::form form;
+    nana::API::enable_dropfiles(form, true);
+    form.events().mouse_dropfiles([](const nana::arg_dropfiles& arg){
+        for (auto && file : arg.files)
+        {
+            std::string filename = http::URLDecode(file);
+        }
+    });
     form.show();
+
     nana::exec();
 
     return 0;
